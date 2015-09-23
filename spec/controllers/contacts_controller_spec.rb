4f4d5066069f99be9e88dceea1010e2a451df902
Alex Bannon <alexbannon@gmail.com>
2015-09-23 12:53:49 -0400

    added beginning tests

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -27,4 +27,52 @@ RSpec.describe ContactsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new Contact" do
+        expect {
+          post :create, {:contact => valid_attributes}
+        }.to change(Contact, :count).by(1)
+      end
+
+      it "assigns a newly created contact as @contact" do
+        post :create, {:contact => valid_attributes}
+        expect(assigns(:contact)).to be_a(Contact)
+        expect(assigns(:contact)).to be_persisted
+      end
+
+      it "redirects to the created contact" do
+        post :create, {:contact => valid_attributes}
+        expect(response).to redirect_to(Contact.last)
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+      let(:new_attributes) {
+        {
+          first_name: "Edited",
+          last_name: "Editson",
+          email_address: "edited@edit.com",
+          phone_number: "555-555-5555",
+          company_name: "Editing Inc"
+        }
+      }
+
+      it "updates the requested contact" do
+        contact = Contact.create! valid_attributes
+        put :update, {:id => contact.to_param, :contact => new_attributes}
+        contact.reload
+        expect(assigns(:contact)).to include(:first_name => "Edited")
+      end
+
+      it "assigns the requested contact as @contact" do
+        contact = Contact.create! valid_attributes
+        put :update, {:id => contact.to_param, :contact => valid_attributes}
+        expect(assigns(:contact)).to eq(contact)
+      end
+    end
+  end
+
 end
