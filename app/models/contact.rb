e3fee88eb29be725d27818588456c31f9cab835c
Alex Bannon <alexbannon@gmail.com>
2015-09-23 14:32:29 -0400

    parsing csv file successfully

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,6 +6,13 @@ class Contact < ActiveRecord::Base
     # create the file path
     path = File.join(directory, name)
     # write the file
-    File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+    temp = File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+    puts "path is #{path}"
+
+    CSV.foreach(path, {headers:true, :header_converters => :symbol, :col_sep => "\t"}) do |row|
+      puts row
+      Contact.create! row.to_hash
+    end
   end
+
 end
