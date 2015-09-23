011bdec515772c8765f3dac48355e18d6f262f40
Alex Bannon <alexbannon@gmail.com>
2015-09-23 14:44:01 -0400

    removed uplading file to public directory - data parsed directly in model and added to psql now through rails magic

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,14 @@
 class ContactsController < ApplicationController
 
   def homepage
-
+    @contacts = Contact.all
   end
 
   def upload
     #Opted to use built in rails storing in file system
     #CarrierWave or Paperclip probably preferred in future
-    post = Contact.save(params[:upload])
-    render :text => "File has been uploaded successfully"
+    Contact.save(params[:upload])
+    redirect_to root_url, notice: "Contacts Imported"
   end
 
 
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,7 @@
 class Contact < ActiveRecord::Base
 
   def self.save(upload)
-    name =  upload['datafile'].original_filename
-    directory = "public/data"
-    # create the file path
-    path = File.join(directory, name)
-    # write the file
-    temp = File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
-    puts "path is #{path}"
-
-    CSV.foreach(path, {headers:true, :header_converters => :symbol, :col_sep => "\t"}) do |row|
+    CSV.foreach(upload.path, {headers:true, :header_converters => :symbol, :col_sep => "\t"}) do |row|
       puts row
       Contact.create! row.to_hash
     end
