c7dd1dc73b7109eaa05f0cd6918843bf7581ea64
Alex Bannon <alexbannon@gmail.com>
2015-09-23 15:12:21 -0400

    passing create and index specs, working on update and destroy

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,5 +11,42 @@ class ContactsController < ApplicationController
     redirect_to root_url, notice: "Contacts Imported"
   end
 
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.json { render json: @contact }
+      else
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.json { render json: @contact }
+      else
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+
+
+
+  private
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email_address, :phone_number, :company_name)
+    end
 
 end
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -21,36 +21,39 @@ RSpec.describe ContactsController, type: :controller do
   }
 
   describe "GET #homepage" do
+
     it "assigns all contacts as @contacts" do
       contact = Contact.create! valid_attributes
-      get :index, {}, valid_session
+      get :homepage, {}
       expect(assigns(:contacts)).to eq([contact])
     end
+
   end
 
   describe "POST #create" do
+
     context "with valid params" do
+
       it "creates a new Contact" do
         expect {
-          post :create, {:contact => valid_attributes}
+          post :create, {:contact => valid_attributes, :format => :json}
         }.to change(Contact, :count).by(1)
       end
 
       it "assigns a newly created contact as @contact" do
-        post :create, {:contact => valid_attributes}
+        post :create, {:contact => valid_attributes, :format => :json}
         expect(assigns(:contact)).to be_a(Contact)
         expect(assigns(:contact)).to be_persisted
       end
 
-      it "redirects to the created contact" do
-        post :create, {:contact => valid_attributes}
-        expect(response).to redirect_to(Contact.last)
-      end
     end
+
   end
 
   describe "PUT #update" do
+
     context "with valid params" do
+
       let(:new_attributes) {
         {
           first_name: "Edited",
@@ -63,17 +66,19 @@ RSpec.describe ContactsController, type: :controller do
 
       it "updates the requested contact" do
         contact = Contact.create! valid_attributes
-        put :update, {:id => contact.to_param, :contact => new_attributes}
+        put :update, {:id => contact.to_param, :contact => new_attributes, :format => :json}
         contact.reload
         expect(assigns(:contact)).to include(:first_name => "Edited")
       end
 
       it "assigns the requested contact as @contact" do
         contact = Contact.create! valid_attributes
-        put :update, {:id => contact.to_param, :contact => valid_attributes}
+        put :update, {:id => contact.to_param, :contact => valid_attributes, :format => :json}
         expect(assigns(:contact)).to eq(contact)
       end
+
     end
+
   end
 
   describe "DELETE #destroy" do
@@ -81,7 +86,7 @@ RSpec.describe ContactsController, type: :controller do
     it "destroys the requested contact" do
       contact = Contact.create! valid_attributes
       expect {
-        delete :destroy, {:id => contact.to_param}
+        delete :destroy, {:id => contact.to_param, :format => :json}
       }.to change(Contact, :count).by(-1)
     end
 
