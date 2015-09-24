ef7b583cb43c2445293b0d7ef71f3451209d45b5
Alex Bannon <alexbannon@gmail.com>
2015-09-24 12:24:32 -0400

    DOM sorting by first name, last name, and email done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,11 +30,40 @@ $(document).ready(function(){
     })
   })
 
+  //stylistic toggling of filters
   $(".unselected").click(function(){
-    $(this).attr("class", "selected");
-    $(".selected").click(function(){
-      $(this).attr("class", "unselected");
-    })
+    $(this).toggleClass("selected")
   })
+
+  $("#sort_first_name").click(function(){
+    sortBy(".first_name")
+  });
+
+  $("#sort_last_name").click(function(){
+    sortBy(".last_name")
+  });
+
+  $("#sort_email").click(function(){
+    sortBy(".email")
+  })
+
+  function sortBy(query_identifier){
+    console.log("sorting by "+query_identifier)
+    var contactsContainer = $("#contacts_list")
+    var contacts = $(".contact_row")
+    contacts.sort(function(a, b){
+      var an = a.querySelector(query_identifier).innerHTML
+      var bn = b.querySelector(query_identifier).innerHTML
+
+      if(an > bn) {
+        return 1;
+      }
+      if(an < bn) {
+        return -1;
+      }
+      return 0;
+    })
+    contacts.detach().appendTo(contactsContainer)
+  }
 
 })
