d6293be955fb8ad3701d78d7262719f24eb4811c
Alex Bannon <alexbannon@gmail.com>
2015-09-24 19:03:13 -0400

    prevent double uploading of contacts and only allow strong params uploads

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,8 +6,16 @@ class Contact < ActiveRecord::Base
   validates :company_name, presence: true
   def self.save(upload)
     CSV.foreach(upload.path, {headers:true, :header_converters => :symbol, :col_sep => "\t"}) do |row|
-      puts row
-      Contact.create! row.to_hash
+      contact = find_by(first_name: row[:first_name]) || new
+
+      #if uploaded contact is not exactly the same, then save as new contact
+      if !(contact.last_name == row[:last_name] && contact.email_address == row[:email_address] && contact.phone_number == row[:phone_number])
+        contact = new
+      end
+
+      #only accept strong params
+      contact.attributes = row.to_hash.slice(:first_name, :last_name, :email_address, :phone_number, :company_name)
+      contact.save!
     end
   end
 
