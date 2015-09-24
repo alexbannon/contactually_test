981064b0d92bb1a4958d93f953983530422ca72f
Alex Bannon <alexbannon@gmail.com>
2015-09-24 13:13:15 -0400

    all filters and sorts minus international working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,22 +30,30 @@ $(document).ready(function(){
     })
   })
 
-  //stylistic toggling of filters
-  $(".unselected").click(function(){
-    $(this).toggleClass("selected")
-  })
-
+  //jquery and vanilla js sorting of DOM
   $("#sort_first_name").click(function(){
+    clearSortBySelections()
+    $("#sort_first_name").attr("class", "selected");
     sortBy(".first_name")
   });
 
   $("#sort_last_name").click(function(){
+    clearSortBySelections()
+    $("#sort_last_name").attr("class", "selected");
     sortBy(".last_name")
   });
 
   $("#sort_email").click(function(){
+    clearSortBySelections()
+    $("#sort_email").attr("class", "selected");
     sortBy(".email")
   })
+
+  function clearSortBySelections(){
+    $("#sort_first_name").attr("class", "unselected");
+    $("#sort_last_name").attr("class", "unselected");
+    $("#sort_email").attr("class", "unselected");
+  }
 
   function sortBy(query_identifier){
     console.log("sorting by "+query_identifier)
@@ -65,5 +73,74 @@ $(document).ready(function(){
     })
     contacts.detach().appendTo(contactsContainer)
   }
+
+  //filtering DOM by international #, ext, and .com
+  function showOrHideNumsWithoutExtension(){
+    var contacts = document.getElementsByClassName("contact_row");
+    var forEach = Array.prototype.forEach;
+    forEach.call(contacts, function(contact){
+      var phone = contact.querySelector(".phone_number").innerHTML
+      if(phone.indexOf("x") == -1){
+        if(contact.style.display == "none"){
+          contact.style.display = "block"
+        }
+        else{
+          contact.style.display = "none"
+        }
+      }
+    })
+  }
+
+  function showOrHideNonComEmails(){
+    var contacts = document.getElementsByClassName("contact_row");
+    var forEach = Array.prototype.forEach;
+    forEach.call(contacts, function(contact){
+      var email = contact.querySelector(".email").innerHTML
+      if(email.substr(email.length - 4) != ".com"){
+        if(contact.style.display == "none"){
+          contact.style.display = "block"
+        }
+        else{
+          contact.style.display = "none"
+        }
+      }
+    })
+  }
+
+  function showOrHideNonInternationalNums(){
+    var contacts = document.getElementsByClassName("contact_row");
+    var forEach = Array.prototype.forEach;
+    forEach.call(contacts, function(contact){
+      var phone = contact.querySelector(".phone_number").innerHTML
+      if(phone.indexOf("x") == -1){
+      }
+    })
+
+  }
+
+
+  function clearFilterSelections(){
+    $("#extension").attr("class", "unselected");
+    $("#email_com").attr("class", "unselected");
+    $("#international").attr("class", "unselected");
+  }
+
+
+  $("#extension").click(function(){
+    clearFilterSelections()
+    $("#extension").attr("class", "selected")
+    showOrHideNumsWithoutExtension()
+  })
+
+  $("#email_com").click(function(){
+    clearFilterSelections()
+    $("#email_com").attr("class", "selected")
+    showOrHideNonComEmails();
+  })
+
+  $("#international").click(function(){
+    clearFilterSelections()
+    $("#international").attr("class", "selected")
+  })
 
 })
