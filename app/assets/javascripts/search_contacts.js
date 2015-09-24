73039ef05936ed4465511c9e72cd5455629a6b34
Alex Bannon <alexbannon@gmail.com>
2015-09-24 18:22:12 -0400

    added search functionality - works on name, email, and phone

---
+++ app/assets/javascripts/search_contacts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $("#search_input").on("keyup", searchContacts)
+
+  $('input[type=search]').on('search', searchContacts)
+
+  function searchContacts(){
+  var input = $("#search_input").val()
+  var search_term = new RegExp(input, 'i')
+  var contacts = document.getElementsByClassName("contact_row");
+  var forEach = Array.prototype.forEach;
+  forEach.call(contacts, function(contact){
+    var phone = contact.querySelector(".phone_number").innerHTML;
+    var email = contact.querySelector(".email").innerHTML;
+    var first_name = contact.querySelector(".first_name").innerHTML;
+    var last_name = contact.querySelector(".last_name").innerHTML;
+    if(phone.search(search_term) != -1){
+      contact.style.display = "block"
+      return
+    }
+    if(email.search(search_term) != -1){
+      contact.style.display = "block"
+      return
+    }
+    if(first_name.search(search_term) != -1){
+      contact.style.display = "block"
+      return
+    }
+    if(last_name.search(search_term) != -1){
+      contact.style.display = "block"
+      return
+    }
+    contact.style.display = "none"
+    })
+  }
+})
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     #Opted to use built in rails storing in file system
     #CarrierWave or Paperclip probably preferred in future
     Contact.save(params[:upload])
-    redirect_to root_url, notice: "Contacts Imported"
+    redirect_to "/contacts", notice: "Contacts Imported"
   end
 
   def index
